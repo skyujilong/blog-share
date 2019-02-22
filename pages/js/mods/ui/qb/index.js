b7ef3bf7366494c7871e42f851502b08d7589dd0
skyujilong <skyujilong@163.com>
2019-02-22 10:38:13 +0800

    添加测试内容

---
+++ pages/js/mods/ui/qb/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let qqLib = '//jsapi.qq.com/get?api=app.share';
+
+let script = document.createElement('script');
+script.src = qqLib;
+var UA = navigator.userAgent;
+script.onload = ()=>{
+    // alert('load......');
+    // var UA = navigator.appVersion;
+    // alert(UA.split("MQQBrowser/").length > 1);
+    // alert(browser.app);
+    browser.app.share({
+        title: '11111',
+        description: '22222',
+        url: location.href,
+        img_url: 'http://n.sinaimg.cn/blog/0977ce10/20171220/blogShareLogo.png',
+        // from: 'web',
+        to_app: 1
+    });
+}
+document.body.append(script);
+
+let sp = document.createElement('span');
+sp.innerHTML = UA;
+document.body.append(sp);
+// alert(1111);
+// alert(browser.app);
+
+// browser.app.share({
+//     title: 11111,
+//     description: 22222,
+//     url: location.href,
+//     img_url: '',
+//     from: 'web',
+//     to_app: 1
+// });
+
+
--- pages/js/page/index.js
+++ pages/js/page/index.js
@@ -1,6 +1,9 @@
 'use strict';
+import '../../scss/base.scss';
 console.log('index..................');
 
+// alert('2333');
+import '../mods/ui/qb';
 
 if(module.hot){
     module.hot.accept();
